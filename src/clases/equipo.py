e9bf68ff78f27747da0489dff3419a4e816b92da
Eduardo <jerogra@gmail.com>
2025-11-17 20:49:03 -0600

    Feat: Jugador y Equipo
    Se agrego modelado POO de jugador y equipo

--- src/clases/equipo.py
+++ src/clases/equipo.py
@@ -1,2 +1,25 @@
+from typing import List
+from src.clases.jugador import Jugador
+
 class Equipo:
-    pass
+    """
+    Representa un equipo. Contiene una lista de objetos Jugador.
+    """
+    LIGA = "Premier League"  # Atributo fijo
+
+    def __init__(self, name: str, jugadores: List[Jugador]):
+        self.__name = name
+        self.__jugadores = jugadores
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def jugadores(self):
+        return self.__jugadores
+
+    @property
+    def total_jugadores(self) -> int:
+        return len(self.__jugadores)
+
--- src/clases/jugador.py
+++ src/clases/jugador.py
@@ -1,7 +1,46 @@
 import pandas as pd
 
 class Jugador:
-    def __init__(self, fabricante, modelo, anno):
-        self.fabricante = fabricante
-        self.modelo = modelo
-        self.anno = anno
+    def __init__(self, name, team, position, age, minutes, goals, assists):
+        self.__name = name
+        self.__team = team
+        self.__position = position
+        self.__age = age
+        self.__minutes = minutes
+        self.__goals = goals
+        self.__assists = assists
+
+@property
+def name(self):
+    return self.__name
+
+
+@property
+def team(self):
+    return self.__team
+
+
+@property
+def goals(self):
+    return self.__goals
+
+
+@goals.setter
+def goals(self, nuevo_valor: int):
+    if nuevo_valor >= 0:
+        self.__goals = nuevo_valor
+    else:
+        raise ValueError("El valor de goles no puede ser negativo.")
+
+
+@property
+def minutes(self):
+    return self.__minutes
+
+
+@minutes.setter
+def minutes(self, nuevo_valor: float):
+    if nuevo_valor >= self.__minutes:
+        self.__minutes = nuevo_valor
+    else:
+        print("Advertencia: Los minutos solo deben ser incrementados.")
